a904393388776b9affbe40d4a883fc310173bf67
Erick Giraldo <erickgiraldo94@gmail.com>
2024-08-25 13:07:40 -0500

    Created entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { ProductsModule } from './products/products.module';
+import { ProducOrdersModule } from './productOrders/productOrders.module';
+import { OrdersModule } from './orders/orders.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { EntitiesModule } from './entities/entities.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/users.entity';
+import { Role } from './roles/entities/roles.entity';
+import { Product } from './products/entities/products.entity';
+import { ProductOrder } from './productOrders/entities/productOrders.entity';
+import { Order } from './orders/entities/orders.entity';
+import { Permission } from './permissions/entities/permissions.entity';
+import { Entities } from './entities/entities/entities.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule, RolesModule, ProductsModule, ProducOrdersModule, OrdersModule, PermissionsModule, EntitiesModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      username: "root",
+      password: "12345678",
+      database: "e_commerce_nest",
+      port: 3306,
+      host: "localhost",
+      synchronize: true,
+      entities: [User, Role, Product, ProductOrder, Order, Permission, Entities],
+    })
+  ],
   // controllers: [AppController],
   // providers: [AppService],
 })
